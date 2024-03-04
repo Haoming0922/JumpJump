a97627276210bf299b0569ad9f87f11e21c4c13f
s220053 <s220053@student.dtu.dk>
2024-03-04 01:29:06 +0100

    update

---
+++ Assets/GameManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public LevelGenerator levelGenerator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelGenerator.GenerateGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
